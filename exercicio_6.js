c91937d35b4cd73779aa86bb0cd27518dc4eb82b
BhryanS2 <bhryanstepenhen@gmail.com>
2022-08-01 10:45:06 -0300

    fix: 🩹  Tirando zero do retorno

--- exercicio_6.js
+++ exercicio_6.js
@@ -1,8 +1,8 @@
-function returnEvenNumbers(list = []) {
-  const evenNumbers = list.filter((number) => number % 2 === 0); // em cpp é: number & 1 === 0
+function returnNumbersDivisibleByTwo(list = []) {
+  const evenNumbers = list.filter((number) => number % 2 === 0 && number !== 0); // em cpp é: number & 1 === 0
   return evenNumbers;
 }
 
 const list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-const evenNumbers = returnEvenNumbers(list);
+const evenNumbers = returnNumbersDivisibleByTwo(list);
 console.log(evenNumbers);
